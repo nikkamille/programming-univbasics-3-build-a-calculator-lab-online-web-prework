3ff0885940759acfa88916d932a17d8637bc76cd
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-01 06:07:30 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,5 +1,5 @@
 def addition(num1, num2)
-  addition = (5 + 4)
+  addition = 5 + 4
   p addition
 end
 
@@ -9,17 +9,21 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  division = 34 / 5
+  p division
 end
 
 def multiplication(num1, num2)
-
+  multiplication = 4 * 30
+  p multiplication
 end
 
 def modulo(num1, num2)
-
+  modulo = 34.0 / 5.0
+  p modulo
 end
 
 def square_root(num)
-
+  square_root = 81.sqrt
+  p square_root
 end
